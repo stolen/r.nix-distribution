0f167d914485ae8ab0c88d578ba5600845004ae5
Danil Zagoskin <z@gosk.in>
2024-05-12 23:18:31 +0300

    wip generic panel driver

--- packages/kernel/drivers/generic-dsi/package.mk
+++ packages/kernel/drivers/generic-dsi/package.mk
@@ -7,6 +7,7 @@ PKG_LICENSE="BSD"
 #PKG_URL="${PKG_SITE}/archive/refs/tags/v${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="generic DSI panel driver"
 PKG_DEPENDS_TARGET="toolchain linux"
+PKG_DEPENDS_INIT="toolchain"
 PKG_NEED_UNPACK="${LINUX_DEPENDS}"
 PKG_TOOLCHAIN="manual"
 PKG_IS_KERNEL_PKG="yes"
@@ -19,4 +20,15 @@ make_target() {
 makeinstall_target() {
   mkdir -p ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}
   cp *.ko ${INSTALL}/$(get_full_module_dir)/${PKG_NAME}/
+}
+
+make_init() {
+  #make_target
+}
+
+makeinstall_init() {
+  mkdir -p ${INSTALL}/lib
+  cp -av ${PKG_DIR}/initramfs/* ${INSTALL}/
+  cp -av ${PKG_DIR}/firmware ${INSTALL}/lib/
+  #cp *.ko ${INSTALL}/lib/modules/
 }
