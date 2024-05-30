37a87005132aa0ec28ec14cf4e4b64463c6a58d0
Danil Zagoskin <z@gosk.in>
2024-05-31 02:44:55 +0300

    generic dsi panel support: generate useful modes

--- packages/kernel/drivers/generic-dsi/package.mk
+++ packages/kernel/drivers/generic-dsi/package.mk
@@ -7,6 +7,14 @@ PKG_LICENSE="GPL"
 PKG_LONGDESC="generic DSI panel driver"
 PKG_TOOLCHAIN="manual"
 
+### How to use this driver
+# 1. Use `scripts/stock2desc.py` on a stock dtb to get a panel description at stdout
+# 2. Use `scripts/desc2dtbo.sh` to wrap a panel description into .dtbo (at stdout)
+# 3. Apply a .dtbo as an FDT overlay in U-boot
+
+### For example, import all new panels from ArkOS repo (https://github.com/AeolusUX/R36S-DTB):
+# for pd in "../R36S-DTB/New Screens"/*; do packages/kernel/drivers/generic-dsi/scripts/stock2desc.py \
+#   "$pd/rk3326-r35s-linux.dtb" -n "R36s New $(basename "$pd")" > packages/kernel/drivers/generic-dsi/firmware/r36s-panel${pd##* }.desc; done
 
 ### For development. Easier to check if code compiles and works.
 ### No need for a long build-transfer-reboot-wait-reboot-check loop
--- packages/kernel/drivers/generic-dsi/scripts/stock2desc.py
+++ packages/kernel/drivers/generic-dsi/scripts/stock2desc.py
@@ -3,11 +3,19 @@
 # https://pypi.org/project/fdt/
 # pip install fdt
 
-import sys
+import sys, argparse
 import fdt
+import math
 
-with open(sys.argv[1], "rb") as f:
+parser = argparse.ArgumentParser(description="Extract MIPI panel description from stock dtb to use with panel-mipi-generic driver")
+parser.add_argument("-n", "--name", help="human readable panel name")
+parser.add_argument(metavar="/path/to/vendor.dtb", dest="source_dtb", help="dtb file from stock firmware")
+args = parser.parse_args()
+
+with open(args.source_dtb, "rb") as f:
     dtb_data = f.read()
+
+g_name = f"name='{args.name}' " if args.name else ""
 
 dt = fdt.parse_dtb(dtb_data)
 panel = dt.get_node("dsi@ff450000/panel@0")
@@ -30,12 +38,14 @@ flags = panel.get_property("dsi,flags").value
 flags |= 0x0400
 
 # G size=52,70 delays=2,1,20,120,50,20 format=rgb888 lanes=4 flags=0xe03
-print(f"G size={w},{h} delays={delays_str} format={fmt} lanes={lanes} flags=0x{flags:x}\n")
+print(f"G {g_name}size={w},{h} delays={delays_str} format={fmt} lanes={lanes} flags=0x{flags:x}\n")
 
 
 timings = panel.get_subnode("display-timings")
 native = timings.get_property("native-mode").value
-# M clock=27500 horizontal=640,50,2,90 vertical=480,18,2,8
+
+# Collect vendor modes
+modes = {}
 for m in timings.nodes:
     clock = round(m.get_property("clock-frequency").value/1000)
     hor = [
@@ -44,19 +54,82 @@ for m in timings.nodes:
             m.get_property("hsync-len").value,
             m.get_property("hback-porch").value,
             ]
-    hor_str = ','.join(map(str, hor))
     ver = [
             m.get_property("vactive").value,
             m.get_property("vfront-porch").value,
             m.get_property("vsync-len").value,
             m.get_property("vback-porch").value,
             ]
-    ver_str = ','.join(map(str, ver))
-    maybe_default = ""
-    if (m.get_property("phandle").value == native):
-        maybe_default = " default=1"
 
-    print(f"M clock={clock} horizontal={hor_str} vertical={ver_str} {maybe_default}")
+    mode = {'clock': clock, 'hor': hor, 'ver': ver}
+    if (m.get_property("phandle").value == native):
+        mode['default'] = True
+
+    htotal = sum(hor)
+    vtotal = sum(ver)
+    fps = clock*1000/(htotal*vtotal)
+
+    if fps not in modes:
+        modes[fps] = mode
+    if (m.get_property("phandle").value == native):
+        modes[fps]['default'] = True
+
+def absfrac(x):
+    return abs(x - round(x))
+
+# Based on vendor modes construct a better set of modes
+# https://tasvideos.org/PlatformFramerates
+# 50, 60        -- generic
+# */1.001       -- NTSC hack with 1001 divisor
+# 50.0070       -- PAL NES  https://www.nesdev.org/wiki/Cycle_reference_chart
+# 60.0988       -- NTSC NES
+# 54.8766       -- src/mame/toaplan/twincobr.cpp
+# 57.5          -- src/mame/kaneko/snowbros.cpp
+# 59.7275       -- https://en.wikipedia.org/wiki/Game_Boy
+# 75.47         -- https://ws.nesdev.org/wiki/Display
+def_fps = 60
+for targetfps in [50/1.001, 50, 50.0070, 57.5, 59.7275, 60/1.001, def_fps, 60.0988, 75.47, 90, 120]:
+    warn = ""
+    # nearest fps to base on
+    greaterfps = [fps for fps in modes.keys() if fps >= targetfps]
+    if greaterfps == []:
+        basefps = max(modes.keys())
+        basemode = modes[basefps]
+        clock = None
+    else:
+        # Trust original clock. If real clock differs, maybe make a whitelist or blacklist here
+        basefps = min(greaterfps)
+        basemode = modes[basefps]
+        clock = basemode['clock']
+    hor = basemode['hor'].copy()
+    ver = basemode['ver'].copy()
+    # Assume original totals are minimal for the panel at this clock
+    htotal = sum(hor)
+    vtotal = sum(ver)
+    if not clock:
+        warn = "(CAN FAIL) "
+        # This may fail, but worth trying. Round up to whole MHz
+        idealclock = targetfps*htotal*vtotal/1000
+        clock = math.ceil(idealclock/1000)*1000
+    maxvtotal = math.floor(clock*1000/targetfps/htotal)
+    # A little bruteforce to find a best totals for target fps
+    # TODO: maybe iterate over some clock values too
+    options = [(absfrac(clock*1000/targetfps/vt), vt) for vt in range(vtotal, min(vtotal+50, maxvtotal+1))]
+    if options == []:
+        print(f"# failed to find mode for fps={targetfps:.6f} c={clock} h={htotal} v={vtotal}")
+        continue
+    (mindev, newvtotal) = min(options)
+    # construct a new mode with chosen vtotal
+    newhtotal = round(clock*1000/targetfps/newvtotal)
+    addhtotal = newhtotal - htotal
+    addvtotal = newvtotal - vtotal
+    expectedfps = clock*1000/newvtotal/newhtotal
+    hor[2] += addhtotal
+    ver[2] += addvtotal
+    hor_str = ','.join(map(str, hor))
+    ver_str = ','.join(map(str, ver))
+    maybe_default = " default=1" if targetfps == def_fps else ""
+    print(f"M clock={clock} horizontal={hor_str} vertical={ver_str}{maybe_default} # {warn}fps={expectedfps:.6f} (target={targetfps:.6f})")
 
 print()
 
